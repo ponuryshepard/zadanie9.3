e70a2a39e81c7dc3dfca659b10428c4f09a4e400
patryk <perseusz91@gmail.com>
2017-05-02 00:05:47 +0200

    słonie

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var text = 'Papugi kosz¹ trawê, unosz¹c siê nad ziemi¹ 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone s³onie';
+var animalUpperCased = animal.toUpperCase('Zielone s³onie');
+
+var textCharsAfter = text.replace('Papugi', 'Zielone s³onie');
