f3e922ad7fb3f2bc22c6f54b8b56e564c71f3a9a
patryk <perseusz91@gmail.com>
2017-05-03 17:42:08 +0200

    papugi

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var text = 'Papugi kosz¹ trawê, unosz¹c siê nad ziemi¹ 15 centymetrów, na czerwonych dywanach.';
-var animal = 'Zielone s³onie';
-var animalUpperCased = animal.toUpperCase('Zielone s³onie');
+var elephant = 'Zielone s³onie';
+var bigElephant = elephant();
 
-var textCharsAfter = text.replace('Papugi', 'Zielone s³onie');
-var partOftext = text.slice(length/2);
+
+var NowyText = text.replace('Papugi', bigElephant);
+
+var partOftext = NowyText.slice(0, NowyText.length/2);
