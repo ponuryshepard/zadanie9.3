2a1bc3bc6af9a8aeefa0362534b947b597e01840
patryk <perseusz91@gmail.com>
2017-05-04 10:44:04 +0200

    nowepapugi

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 var text = 'Papugi kosz¹ trawê, unosz¹c siê nad ziemi¹ 15 centymetrów, na czerwonych dywanach.';
 var elephant = 'Zielone s³onie';
-var bigElephant = elephant();
+var elephantUpperCased = elephant.toUpperCase();
 
 
-var NowyText = text.replace('Papugi', bigElephant);
+var NowyText = text.replace('Papugi', elephantUpperCased);
 
 var partOftext = NowyText.slice(0, NowyText.length/2);
+
+console.log(partOftext);
